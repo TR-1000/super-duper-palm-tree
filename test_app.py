839cc8941d3e4b5895e6eb57bedee58d7e8f096c
TR-1000 <t.l.ross@outlook.com>
2026-09-08 17:00:33 -0500

    Testing unit test failure

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b'<body style="background-color: black; color: white;">Hello from my DevOps pipeline!</body>'
+    assert response.data == b"Hello from my DevOps pipeline!"
 
 
 def test_health():
